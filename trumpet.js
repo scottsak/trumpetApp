156daf29843d1bd049d948ceccf837fc66b6661f
scottsak <48599109+scottsak@users.noreply.github.com>
2022-01-28 22:03:19 -0800

    added some functions

--- trumpet.js
+++ trumpet.js
@@ -1,75 +1,77 @@
+
+
 for(var i = 0; i<document.querySelectorAll(".notes").length;i++){
 
     document.querySelectorAll(".notes")[i].addEventListener("click", buttonPressed);
-  
+
   }
-  
+
   document.addEventListener("keydown", keyPressed);
-  
+
   function keyPressed(event){
+    console.log(event);
     makeSound(event.key);
     buttonAnimation(event.key);
   }
-  
+
   function buttonPressed(){
-    var chord = this.innerHTML;
-    console.log(chord);
+    var chord = this.id;
     makeSound(chord);
     buttonAnimation(chord);
   }
-  
-  
+
+
   function makeSound(key){
     switch (key) {
       case "1":
-        var cLow = new Audio("sounds/cLow.mp3");
+        var cLow = new Audio("sounds/cLow.m4a");
         cLow.play();
         break;
       case "2":
-        var dLow = new Audio("sounds/dLow.mp3");
+        var dLow = new Audio("sounds/dLow.m4a");
         dLow.play();
         break;
       case "3":
-        var fLow = new Audio("sounds/fLow.mp3");
+        var fLow = new Audio("sounds/fLow.m4a");
         fLow.play();
         break;
       case "4":
-        var gLow = new Audio("sounds/gLow.mp3");
+        var gLow = new Audio("sounds/gLow.m4a");
         gLow.play();
         break;
       case "5":
-        var gLow = new Audio("sounds/gLow.mp3");
+        var gLow = new Audio("sounds/gLow.m4a");
         gLow.play();
         break;
       case "6":
-        var aHigh = new Audio("sounds/aHigh.mp3");
+        var aHigh = new Audio("sounds/aHigh.m4a");
         aHigh.play();
         break;
       case "7":
-        var bHigh= new Audio("sounds/bHigh.mp3");
+        var bHigh= new Audio("sounds/bHigh.m4a");
         bHigh.play();
         break;
       case "8":
-        var cHigh = new Audio("sounds/cHigh.mp3");
+        var cHigh = new Audio("sounds/cHigh.m4a");
         cHigh.play();
         break;
       case "9":
-        var dHigh = new Audio("sounds/dHigh.mp3");
+        var dHigh = new Audio("sounds/dHigh.m4a");
         dHigh.play();
         break;
       case "0":
-        var eHigh = new Audio("sounds/eHigh.mp3");
+        var eHigh = new Audio("sounds/eHigh.m4a");
         eHigh.play();
         break;
       default:
         break;
       }
   }
-  
+
   function buttonAnimation(eventkey){
     var activeButton = document.querySelector("." + eventkey);
     activeButton.classList.add("pressed");
-  
+
     setTimeout(function(){
       activeButton.classList.remove("pressed");
     },100);
